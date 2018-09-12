432cac6ab38c7a00190c5cf329a719670d7673b7
Nikolay Tomitov <ntomitov@gmail.com>
2018-09-12 15:26:06 +0300

    get feed

--- backend/routes/posts/PostsController.js
+++ backend/routes/posts/PostsController.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const INSERT_POST_QUERY = 'INSERT INTO posts VALUES (null, ?, ?, ?)';
+const USER_FEED_QUERY = 'SELECT p.id, p.text, p.timestamp '+
+'FROM posts p '+
+'LEFT OUTER JOIN user_follows_user uf ON '+
+'(uf.followed_id = p.user_id AND uf.follower_id = ?) '+
+'WHERE uf.followed_id is not null;';
 
 router.post('/', function (req, res, next) {
   const { userId } = req;
@@ -11,6 +16,14 @@ router.post('/', function (req, res, next) {
   .then(results => res.json({"status": 200, "error": null, 
     "data": { id : results.insertId }
   }))
+  .catch(error => res.json({"status": 500, "error": error, "data": null}));
+});
+
+router.get('/feed', function (req, res, next) {
+  const { userId } = req;
+  
+  res.locals.db.query(USER_FEED_QUERY, [userId])
+  .then(results => res.json({"status": 200, "error": null, "data": results}))
   .catch(error => res.json({"status": 500, "error": error, "data": null}));
 });
 
